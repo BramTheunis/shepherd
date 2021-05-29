81867ec9a8ddbb2aa81372aeeda9835d2ec561cd
Bram Theunis <bramtheunis1997@gmail.com>
2021-05-30 00:21:56 +0200

    HTTPS/HTTP feature now only works if feature is actually turned on by the user

--- js/background.js
+++ js/background.js
@@ -2,9 +2,9 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.protocol == "http")
         chrome.notifications.create("", {
-            title: "titeltje",
-            message: "bericht zelf",
-            iconUrl: "img/lock.png",
+            title: "HTTPS alert",
+            message: "Deze website maakt GEEN gebruik van HTTPS. Verzonden gegevens zijn daarom mogelijk niet goed beveiligd.",
+            iconUrl: "img/logo128.png",
             type: "basic",
             priority: 2
         });
--- js/https.js
+++ js/https.js
@@ -20,18 +20,20 @@ switchBtnHTTPS.addEventListener("click", function() {
 // END OF SWITCH FUNCTION
 
 // START OF HTTPS DETECTION FUNCTION
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+if (httpsValue == "On") {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 
-// Variabelen definiëren voor protocol bepaling
-    let protoc = tabs[0].url.split(":")[0]
-
-    // Wat moet er gebeuren bij veilige website, wat bij onveilige website
-    if (protoc === "https") {
-        console.log("safe");
-    } else if (protoc ===  "http") {
-        chrome.runtime.sendMessage({protocol: "http"}, function(response) {
-            console.log("not so safe here");
-        });
-    }
-});
+    // Variabelen definiëren voor protocol bepaling
+        let protoc = tabs[0].url.split(":")[0]
+    
+        // Wat moet er gebeuren bij veilige website, wat bij onveilige website
+        if (protoc === "https") {
+            console.log("safe");
+        } else if (protoc ===  "http") {
+            chrome.runtime.sendMessage({protocol: "http"}, function(response) {
+                
+            });
+        }
+    });
+}
 // END OF FUNCTION
